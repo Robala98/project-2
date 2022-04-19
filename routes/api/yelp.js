5273c865263d252df36eaff7c9f89005db141b1c
Robala <robala998@gmail.com>
2022-04-19 13:25:50 -0500

    yelp handler is working

--- routes/api/yelp.js
+++ routes/api/yelp.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 // POST METHOD ON /api/yelp
 router.post('/', async (req, res) => {
+  var input = req.body.search
+  console.log(input)
     var config = {
       method: 'get',
-      url: 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972',
+      url: `https://api.yelp.com/v3/businesses/search?term=${input}&latitude=37.786882&longitude=-122.399972`,
       headers: { 
         'Authorization': 'Bearer UAuVrLrHQLfdqg-qf9iBPIt5v1PVTT1ZwsZWcw6KajyMdQMMZ1cCreeHfJJKsyNerB7jboINNW4kkVJ3Y9RcEa01QSm6vldjzMO6kiOTfh8JAQ67OOpnVq_P3flWYnYx'
       }
